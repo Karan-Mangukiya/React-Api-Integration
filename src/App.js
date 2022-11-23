b291fdb86d1238243804a2acce07b527cd168135
chandresh8187 <borichachandresh35@gmail.com>
2022-11-23 10:45:52 +0530

    get-method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://official-joke-api.appspot.com/random_joke").then((result) => {
+      result.json().then((resp) => {
+        // console.log("result", resp);
+        setData(resp);
+      });
+    });
+  }, []);
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get Api Call</h1>
+      <table border="1">
+        <tr>
+          <td>ID</td>
+          <td>Type</td>
+          <td>Setup</td>
+          <td>Punchline</td>
+        </tr>
+
+        {/* {data.map((item) => (
+          <tr>
+            <td>{item.id}</td>
+            <td>{item.type}</td>
+            <td>{item.setup}</td>
+            <td>{item.punchline}</td>
+          </tr>
+        ))} */}
+      </table>
     </div>
   );
 }
